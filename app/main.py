4267cc558b030577082c989f611092f2ffc81c88
ZanZvokelj <zan.zvokelj@academiavss.si>
2026-01-14 17:09:25 +0100

    Fix CORS for frontend

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+"https://fitness-app-backend-c53u.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
