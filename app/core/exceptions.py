bf8842dc10eb37f37e5615122a232d58a419c418
ZanZvokelj <zan.zvokelj@academiavss.si>
2026-01-18 18:54:41 +0100

    final clean up

---
+++ app/core/exceptions.py
@@ -0,0 +1,26 @@
+# app/core/exceptions.py
+from fastapi import Request, HTTPException
+from fastapi.responses import JSONResponse
+from slowapi.errors import RateLimitExceeded
+
+def register_exception_handlers(app):
+    @app.exception_handler(HTTPException)
+    async def http_exception_handler(request: Request, exc: HTTPException):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"detail": exc.detail},
+        )
+
+    @app.exception_handler(RateLimitExceeded)
+    async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+        return JSONResponse(
+            status_code=429,
+            content={"detail": "Too many requests"},
+        )
+
+    @app.exception_handler(Exception)
+    async def unhandled_exception_handler(request: Request, exc: Exception):
+        return JSONResponse(
+            status_code=500,
+            content={"detail": "Internal server error"},
+        )
--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from app.core.exceptions import register_exception_handlers
 
 from slowapi.middleware import SlowAPIMiddleware
-from slowapi.errors import RateLimitExceeded
 
 from app.core.limiter import limiter
 from app.core.config import settings
@@ -24,16 +24,11 @@ from app.routers import (
 
 app = FastAPI(title="Group Fitness Booking API")
 
+register_exception_handlers(app)
+
 # attach limiter
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
-
-@app.exception_handler(RateLimitExceeded)
-def rate_limit_handler(request: Request, exc: RateLimitExceeded):
-    return JSONResponse(
-        status_code=429,
-        content={"detail": "Too many requests. Please slow down."},
-    )
 
 app.add_middleware(
     CORSMiddleware,
